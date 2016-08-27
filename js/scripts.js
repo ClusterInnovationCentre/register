5518592d114422f42b8e4bfeb0e348862dbd74b6
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-08-27 13:23:54 +0530

    Registration done.

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,12 @@ console.log('      ////         ////                ');
 */
 
 
-$('#course').prop('disabled', true);
-$('#department').prop('disabled', true);
-$('#enroll').prop('disabled', true);
-$('.btn-auth').addClass('disabled');
+// $('#course').prop('disabled', true);
+// $('#department').prop('disabled', true);
+// $('#enroll').prop('disabled', true);
+// $('.btn-auth').addClass('disabled');
 
-$('#type').on('change', function(e) {
+$('#type').on('change', function (e) {
 	var optionSelected = $("option:selected", this);
     var valueSelected = this.value;
 
@@ -45,31 +45,31 @@ $('#type').on('change', function(e) {
 });
 
 // Code to disable auth providers link.
-$(function() {
+$(function () {
 	jQuery.fn.extend({
-		disable: function(state) {
-			return this.each(function() {
+		disable: function (state) {
+			return this.each(function () {
 				var $this = $(this);
 				$this.toggleClass('disabled', state);
 			});
 		}
 	});
 
-	$('body').on('click', 'a.disabled', function(event) {
+	$('body').on('click', 'a.disabled', function (event) {
 		event.preventDefault();
 	});
 });
 
-$('courseForm').submit(function() {
+$('courseForm').submit(function () {
 	return false;
 });
 
 // Allow only numeric input 
-$(function() {
-  $('.mui-textfield').on('keydown', '#enroll', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
+$(function () {
+	$('.mui-textfield').on('keydown', '#enroll', function (e) { -1 !== $.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) || /65|67|86|88/.test(e.keyCode) && (!0 === e.ctrlKey || !0 === e.metaKey) || 35 <= e.keyCode && 40 >= e.keyCode || (e.shiftKey || 48 > e.keyCode || 57 < e.keyCode) && (96 > e.keyCode || 105 < e.keyCode) && e.preventDefault() });
 })
-$(function() {
-  $('.mui-textfield').on('keydown', '#phone', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
+$(function () {
+	$('.mui-textfield').on('keydown', '#phone', function (e) { -1 !== $.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) || /65|67|86|88/.test(e.keyCode) && (!0 === e.ctrlKey || !0 === e.metaKey) || 35 <= e.keyCode && 40 >= e.keyCode || (e.shiftKey || 48 > e.keyCode || 57 < e.keyCode) && (96 > e.keyCode || 105 < e.keyCode) && e.preventDefault() });
 })
 
 localStorage.clear();
@@ -100,100 +100,86 @@ function login(network) {
 		$('a').disable(false);
 		return;
 	}
-	
-	hello(network).login({scope: 'email'});
+
+	hello(network).login({ scope: 'email' });
 }
 
-hello.on('auth.login', function(auth) {
+hello.on('auth.login', function (auth) {
 
 
 	// Call user information, for the given network
-	hello(auth.network).api('/me').then(function(r) {
-		
+	hello(auth.network).api('/me').then(function (r) {
+
 		// Authenticated succesfully.
-		
-		console.log("Network: " + auth.network + ", " + "User: " + r.name + ", " + "E-Mail: " + r.email + ", " + "Response dump: " + JSON.stringify(r) );
-		
+
+		console.log("Network: " + auth.network + ", " + "User: " + r.name + ", " + "E-Mail: " + r.email + ", " + "Response dump: " + JSON.stringify(r));
+
 		// Check if email is available or not.
-		if (r.email != undefined)
-		{
+		if (r.email != undefined) {
 			// Posting to the sheetsu API.
-			$.get("https://sheetsu.com/apis/v1.0/667af32ec687/email/" + r.email,
-				function( data ){
-					if(data.status === 200 && data.success === true)
-					{
-						console.log(data.result);
-						if(($.grep(data.result, function(e){ return e.email === r.email })).length === 0)
-						{
-							var payload = { };
-							payload.name = r.name;
-							payload.network = auth.network;
-							payload.email = r.email;
-							payload.type = $('#type').val();
-							payload.phone = $('#phone').val();
-							payload.extra = $('#extra').val();
-							payload.dump = JSON.stringify(r);
-							payload.year = $('#year').val();
-							payload.timestamp = Date.now();
-							payload.communities = "";
+			$.ajax({
+				url: "https://sheetsu.com/apis/v1.0/667af32ec687/search?email=" + r.email,
+				statusCode: {
+					404: function () {
 
-							if ($('#type') === 'student') {
-								payload.course = $('#course').val();
-							}
-							else {
-								payload.department = $('#department').val();
-							}
 
-							$('#communities').each(function (index) {
-								payload.communities += $(this).is(':checked') ? "," + $(this).val() : "";
-							});
+						var payload = {};
+						payload.name = r.name;
+						payload.network = auth.network;
+						payload.email = r.email;
+						payload.type = $('#type').val();
+						payload.phone = $('#phone').val();
+						payload.extra = $('#extra').val();
+						payload.dump = JSON.stringify(r);
+						payload.year = $('#year').val();
+						payload.timestamp = Date.now();
+						payload.communities = "";
 
-							$.ajax({
-								url: 'https://sheetsu.com/apis/v1.0/667af32ec687',
-								type: 'post',
-								data: {
-									name: r.name,
-									network: auth.network,
-									email: r.email,
-									course: $('#course').val(),
-									year: $('#enroll').val(),
-									phone: $('#phone').val(),
-									extra: $('#extra').val(),
-									response_dump: JSON.stringify(r)
-								},
-								dataType: 'json',
-								success: function(response) {
-									console.log(data);
-									alert("Registered Successfully!");
-									$('a').disable(false);
-								}
-
-							});
+						if ($('#type').val() === 'student') {
+							payload.id = $('#enroll').val();
+							payload.course = $('#course').val();
 						}
-						else
-						{
-							console.log("User already registered.");
-							alert("Already registered!");
-							$('a').disable(false);
-							// TODO - Implement showing previous registration.
+						else {
+							payload.department = $('#department').val();
 						}
+
+						$('#communities > label > input').each(function (index) {
+							payload.communities += $(this).prop('checked') ? $(this).val() + "," : "";
+						});
+
+						$.ajax({
+							url: 'https://sheetsu.com/apis/v1.0/667af32ec687',
+							type: 'post',
+							data: payload,
+							dataType: 'json',
+							success: function (response) {
+								console.log(response);
+								alert("Registered Successfully!");
+								$('a').disable(false);
+							},
+							error: function (jqXHR, response, e) { 
+								console.log(e);
+							 }
+
+						});
 					}
-					else
-					{
-						alert("Unknown error occurred!");
+
+					,
+					200: function () {
+						console.log("User already registered.");
+						alert("Already registered!");
 						$('a').disable(false);
-						console.log(data);
-						// TODO - Implement error showing while connecting to the API.
 					}
-				});
+				}
+
+			});
 		}
-		else
-		{
+		else {
 			alert("Unable to get e-mail ID, please check you profile or try a different provier!!!");
 		}
 	});
-		
-}, function(e) {
+
+}, function (e) {
 	alert('Unknown error occurred!!!');
 	console.log(e);
 	$('a').disable(true);
